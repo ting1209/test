aeeba3d9801b457656a4f3790a5cefbba475f05d
ting1209 <34576020+ting1209@users.noreply.github.com>
2018-12-21 17:10:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='hello world!')
+    message = StickerSendMessage(package_id='1',sticker_id='1')
     line_bot_api.reply_message(event.reply_token, message)
 
 
