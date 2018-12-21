e0cf50b1a1b1aa8ac5c55cacd7173af60d7bb62a
ting1209 <34576020+ting1209@users.noreply.github.com>
2018-12-21 16:31:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='請撥打客服專線')
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 
