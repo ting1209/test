9841f783e79709cbd93e77b20002abad3125a730
ting1209 <34576020+ting1209@users.noreply.github.com>
2018-12-22 21:24:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+
+import errno
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
 from flask import Flask, request, abort
 
 from linebot import (
@@ -30,60 +37,172 @@ def callback():
         abort(400)
     return 'OK'
 
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
+
+@handler.add(MessageEvent, message=TextMessage) # 處理文字訊息（message = TextMessage），圖片
 def handle_message(event):
-    message = TemplateSendMessage(
-    alt_text='ImageCarousel template',
-    template=ImageCarouselTemplate(
-        columns=[
-            ImageCarouselColumn(
-                image_url='https://example.com/item1.jpg',
-                action=PostbackTemplateAction(
-                    label='飯',
-                    text='飯',
-                    data='action=buy&itemid=1'
-                )
+    text = event.message.text # 使用者傳的訊息存成變數 text
+
+    if text == 'buttons' or text == '按鈕':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米') #Messageaction: 替使用者傳訊息，label為選項的文字，text為要傳的訊息
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message) # 送出訊息，訊息內容為'template_message'
+    elif text == '吃吃':
+        carousel_template = CarouselTemplate(imageAspectRatio='rectangle',imageSize='contain',columns=[
+            CarouselColumn(text='大門',thumbnailImageUrl = 'https://i.imgur.com/wT9Rjq7.jpg', actions=[
+                MessageAction(label='飯', text='大門_飯'),
+                MessageAction(label='麵', text='大門_麵'),
+                MessageAction(label='其他', text='大門_其他')
+            ]),
+            CarouselColumn(text='公館',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+                MessageAction(label='飯', text='公館_飯'),
+                MessageAction(label='麵', text='公館_麵'),
+                MessageAction(label='其他', text='公館_其他')
+            ]),
+            CarouselColumn(text='溫州',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+                MessageAction(label='飯', text='溫州_飯'),
+                MessageAction(label='麵', text='溫州_麵'),
+                MessageAction(label='其他', text='溫州_其他')
+            ]),
+            CarouselColumn(text='後門',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+                MessageAction(label='飯', text='後門_飯'),
+                MessageAction(label='麵', text='後門_麵'),
+                MessageAction(label='其他', text='後門_其他')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Carousel alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'image_carousel':
+        image_carousel_template = ImageCarouselTemplate(columns=[
+            ImageCarouselColumn(image_url='https://i.imgur.com/NrFOHGo.jpg',
+                                action=DatetimePickerAction(label='datetime',
+                                                            data='datetime_postback',
+                                                            mode='datetime')),
+            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
+                                action=DatetimePickerAction(label='date',
+                                                            data='date_postback',
+                                                            mode='date'))
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='ImageCarousel alt text', template=image_carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'flex':
+        bubble = BubbleContainer(
+            direction='ltr',
+            hero=ImageComponent(
+                url='https://i.imgur.com/wT9Rjq7.jpg',
+                size='full',
+                aspect_ratio='20:13',
+                aspect_mode='cover',
+                action=URIAction(uri='http://example.com', label='label')
             ),
-            ImageCarouselColumn(
-                image_url='https://example.com/item1.jpg',
-                action=PostbackTemplateAction(
-                    label='麵',
-                    text='麵',
-                    data='action=buy&itemid=2'
-                )
+            body=BoxComponent(
+                layout='vertical',
+                contents=[
+                    # title
+                    TextComponent(text='Brown Cafe', weight='bold', size='xl'),
+                    # review
+                    BoxComponent(
+                        layout='baseline',
+                        margin='md',
+                        contents=[
+                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                            TextComponent(text='4.0', size='sm', color='#999999', margin='md',
+                                          flex=0)
+                        ]
+                    ),
+                    # info
+                    BoxComponent(
+                        layout='vertical',
+                        margin='lg',
+                        spacing='sm',
+                        contents=[
+                            BoxComponent(
+                                layout='baseline',
+                                spacing='sm',
+                                contents=[
+                                    TextComponent(
+                                        text='Place',
+                                        color='#aaaaaa',
+                                        size='sm',
+                                        flex=1
+                                    ),
+                                    TextComponent(
+                                        text='Shinjuku, Tokyo',
+                                        wrap=True,
+                                        color='#666666',
+                                        size='sm',
+                                        flex=5
+                                    )
+                                ],
+                            ),
+                            BoxComponent(
+                                layout='baseline',
+                                spacing='sm',
+                                contents=[
+                                    TextComponent(
+                                        text='Time',
+                                        color='#aaaaaa',
+                                        size='sm',
+                                        flex=1
+                                    ),
+                                    TextComponent(
+                                        text="10:00 - 23:00",
+                                        wrap=True,
+                                        color='#666666',
+                                        size='sm',
+                                        flex=5,
+                                    ),
+                                ],
+                            ),
+                        ],
+                    )
+                ],
             ),
-            ImageCarouselColumn(
-                image_url='https://example.com/item1.jpg',
-                action=PostbackTemplateAction(
-                    label='鍋',
-                    text='鍋',
-                    data='action=buy&itemid=1'
-                )
+            footer=BoxComponent(
+                layout='vertical',
+                spacing='sm',
+                contents=[
+                    # callAction, separator, websiteAction
+                    SpacerComponent(size='sm'),
+                    # callAction
+                    ButtonComponent(
+                        style='link',
+                        height='sm',
+                        action=URIAction(label='CALL', uri='tel:000000'),
+                    ),
+                    # separator
+                    SeparatorComponent(),
+                    # websiteAction
+                    ButtonComponent(
+                        style='link',
+                        height='sm',
+                        action=URIAction(label='WEBSITE', uri="https://example.com")
+                    )
+                ]
             ),
-            ImageCarouselColumn(
-                image_url='https://example.com/item1.jpg',
-                action=PostbackTemplateAction(
-                    label='飲料',
-                    text='飲料',
-                    data='action=buy&itemid=1'
-                )
-            ),
-            ImageCarouselColumn(
-                image_url='https://example.com/item1.jpg',
-                action=PostbackTemplateAction(
-                    label='其他',
-                    text='其他',
-                    data='action=buy&itemid=1'
-                )
-            )
-        ]
-    )
-)
-    line_bot_api.reply_message(event.reply_token, message)
+        )
+        message = FlexSendMessage(alt_text="hello", contents=bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+    else:
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token, message)
 
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
