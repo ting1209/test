7910db9e384efee51ef0b52746a0d9e39a34e156
huang <b04701216@ntu.edu.tw>
2018-12-24 21:59:51 +0800

    add push

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import schedule
+import time
 import errno
 import json
 import os
@@ -23,7 +25,16 @@ line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIA
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
+def job():
+    print("找找看今天吃什麼吧")
+	
+schedule.every().day.at("12:00").do(job)
+schedule.every().day.at("22:02").do(job)
+schedule.every(1).minutes.do(job)
 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
