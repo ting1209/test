fbebc2ef653ea02af263e5212cc897c2b4da9543
huang <b04701216@ntu.edu.tw>
2018-12-25 19:11:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+import schedule
+import time
 import pandas as pd
 import numpy as np
 import errno
@@ -17,6 +19,16 @@ from linebot.exceptions import (
 from linebot.models import *
 
 app = Flask(__name__)
+
+#push
+def noti(message):
+	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
+	return True	
+	
+schedule.every().day.at("19:20").do(noti('來找找今天吃什麼鴨^^'))
+	
+while True:
+	schedule.run_pending()
 
 #
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
