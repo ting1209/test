255e8fbf6f24f59a699e8fb09b923b6b90c70861
huang <b04701216@ntu.edu.tw>
2018-12-25 16:23:35 +0800

    Add push

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
-
+import schedule
+import time
 import errno
 import json
 import os
@@ -23,6 +24,17 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIAj3RErrz2rEtt22mDnnTqZOLlqHYCuN6Aw7TMJ6qkS0cmvICHR5ZcgeczP6VbqCaQz9ezdAy/zsJV6nJSWoFntlnzQMTui9yzQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
+
+
+def job():
+	message = '找找看今天吃什麼吧!'
+	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text=message))
+	
+schedule.every(1).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
 # 監聽所有來自 /callback 的 Post Request
