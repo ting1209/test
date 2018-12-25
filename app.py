b8a5d5ee6b0f9f94a09be96569769ec2ccb5feb1
huang <b04701216@ntu.edu.tw>
2018-12-25 19:52:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,17 +25,6 @@ line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIA
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
-#push
-def noti(message):
-	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
-	return True	
-	
-schedule.every().day.at("19:45").do(noti('來找找今天吃什麼鴨^^'))
-	
-while True:
-	schedule.run_pending()
-	time.sleep(1)
-
 #
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
 def rest_selector(reply_text):
@@ -258,6 +247,17 @@ def handle_message(event):
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
+
+#push
+def noti(message):
+	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
+	return True	
+	
+schedule.every().day.at("19:45").do(noti('來找找今天吃什麼鴨^^'))
+	
+while True:
+	schedule.run_pending()
+	time.sleep(1)
 
 
 if __name__ == "__main__":
