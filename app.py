ae7772e4a820101e6221bf9dfc60e39d85e4d013
huang <b04701216@ntu.edu.tw>
2018-12-25 20:14:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,15 +27,14 @@ handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
 
 #push
-def noti(message):
-	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
-	return True	
+def noti():
+	print('來找找今天吃什麼鴨^^')
 	
-schedule.every().day.at("20:08").do(noti('來找找今天吃什麼鴨^^'))
+schedule.every().day.at("20:17").do(noti)
 	
-while True:
+while 1:
 	schedule.run_pending()
-	break
+	time.sleep(1)
 
 #
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
