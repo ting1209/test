f52fb28c4226f835ba789bcb9978aa5f5c8f4698
huang <b04701216@ntu.edu.tw>
2018-12-26 09:27:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,11 @@ line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIA
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
-
-schedule.every().day.at('09:25').do(line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text ='看看今天吃什麼鴨')))
+def job(message):
+	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text = message))
+	return True
+	
+schedule.every().day.at('09:30').do(job('hi'))
 	
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
 def rest_selector(reply_text):
@@ -254,5 +257,6 @@ def handle_message(event):
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
-	schedule.run_pending()
+	while True:
+		schedule.run_pending()
 	
