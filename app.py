d0c29870c452742266bc27ac6cd2d5543480fffe
huang <b04701216@ntu.edu.tw>
2018-12-26 08:32:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+import schedule
+import time
 import pandas as pd
 import numpy as np
 import errno
@@ -23,8 +25,12 @@ line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIA
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
-line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text ='吃什麼鴨'))
+def job():
+	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text ='吃什麼鴨'))
+	return True
 
+schedule.every().day.at('08:40').do(job)
+	
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
 def rest_selector(reply_text):
     res_loc, res_type = reply_text.split('_')
@@ -249,5 +255,8 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
