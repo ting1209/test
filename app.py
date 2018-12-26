1cec5dfc07e3ae1c5a3573d53cdc1b8bda9344c2
huang <b04701216@ntu.edu.tw>
2018-12-26 09:21:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,8 @@ line_bot_api = LineBotApi('GcXT0hcdzVX8y0VopCEgHKKRKhZL1jKsALAkwxTV49W7dLbq2myIA
 # Channel Secret
 handler = WebhookHandler('a7f676f0726586e8fe40d2a58227ca8a')
 
-def noti(message):
-	line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
-	return True
 
-schedule.every().day.at('09:20').do(noti('看看今天吃什麼鴨^^'))
+schedule.every().day.at('09:25').do(line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text ='看看今天吃什麼鴨')))
 	
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
 def rest_selector(reply_text):
@@ -253,9 +250,9 @@ def handle_message(event):
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
 
-while True:
-	if __name__ == "__main__":
-		port = int(os.environ.get('PORT', 5000))
-		app.run(host='0.0.0.0', port=port)
-		schedule.run_pending()
+
+if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+	schedule.run_pending()
 	
